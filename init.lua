9ff1e3d25b2944e8b591a19359e3fc0887282af3
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-05-14 16:08:04 +0545

    handlers

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.cmd([[colorscheme rose-pine]])
+vim.cmd("highlight Normal guibg=none ctermbg=none")
+vim.cmd("highlight NormalFloat guibg=none ctermbg=none")
